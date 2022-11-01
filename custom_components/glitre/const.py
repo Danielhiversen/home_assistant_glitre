b35d476faf6847999fee2d68c3a99ef73447373b
Daniel Hoyer <mail@dahoiv.net>
2022-11-01 12:27:15 +0000

    fixup! Format Python code with isort

--- custom_components/glitre/const.py
+++ custom_components/glitre/const.py
@@ -1,10 +1,8 @@
 """constants for Tibber integration."""
 
-from homeassistant.components.sensor import (
-    SensorDeviceClass,
-    SensorEntityDescription,
-    SensorStateClass,
-)
+from homeassistant.components.sensor import (SensorDeviceClass,
+                                             SensorEntityDescription,
+                                             SensorStateClass)
 
 DOMAIN = "glitre"
 
